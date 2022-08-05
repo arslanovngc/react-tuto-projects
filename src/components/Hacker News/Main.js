9a4a3aa054b36d87b0acf06bb6329eaf08780db7
arslanov-ngc <developer.humble@gmail.com>
2022-08-05 11:44:14 +0500

    Done. Seventh project's second part

--- src/components/Hacker News/Main.js
+++ src/components/Hacker News/Main.js
@@ -2,6 +2,8 @@ import SearchForm from "./SearchForm";
 import Button from "./Button";
 import Stories from "./Stories";
 
+import "./main.css";
+
 const Main = () => {
   return (
     <>
--- src/components/Hacker News/Stories.js
+++ src/components/Hacker News/Stories.js
@@ -1,12 +1,31 @@
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  const { isLoading: loading, hits, removeStory } = useGlobalContext();
+  const { isLoading, hits, removeStory } = useGlobalContext();
 
-  if (loading) {
-    return <h1 className="loading"></h1>;
+  if (isLoading) {
+    return <div className="loading"></div>;
   }
-  return <div>Stories</div>;
+  return (
+    <section className="stories">
+      {hits.map(({ objectID, title, num_comments, url, points, author }) => {
+        return (
+          <article className="story" key={objectID}>
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              <code>{points}</code> points by <code>{author.toUpperCase()}</code> |<code> {num_comments} </code>comments
+            </p>
+            <div>
+              <a href={url} className="read-link" target="_blank" rel="noreferrer">
+                read more
+              </a>
+              <button className="remove-btn" onClick={() => removeStory(objectID)}>remove</button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stories;
