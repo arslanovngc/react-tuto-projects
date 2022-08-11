88d9d23891db7f8c4647f1bb46ffd778d7242e91
arslanov-ngc <developer.humble@gmail.com>
2022-08-11 12:25:48 +0500

    Done. Nineth project's first part

---
+++ src/components/Github Users Dashboard/pages/Dashboard.js
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { Navbar, Info, Repos, User, Search } from "./../components";
+import loader from "./../images/preloader.gif";
+
+import { GithubContext } from "../context/context";
+
+const Dashboard = () => {
+  const { isLoading } = useContext(GithubContext);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar></Navbar>
+        <Search />
+        <img src={loader} alt="loader" className="loading-img" />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Navbar></Navbar>
+      <Search />
+      <Info />
+      <User />
+      <Repos />
+    </main>
+  );
+};
+
+export default Dashboard;
